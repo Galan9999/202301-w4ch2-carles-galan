84bdc65c7db8b21898b8c58231b8fc1fb518dad6
Carles Galan <galan.carles@gmail.com>
2023-02-01 08:42:05 +0100

    Add ImageDisplay

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,16 @@
+import Form from "./components/Form/Form";
+import ImageDisplay from "./components/ImageDisplay/ImageDisplay";
+
 const App = (): JSX.Element => {
+  const text = "hola";
+
   return (
     <div className="container">
-      <form className="search-form">
-        <div className="row">
-          <label htmlFor="search" className="col-1 col-form-label">
-            Search:{" "}
-          </label>
-          <div className="col-4">
-            <input type="text" id="search" className="search form-control" />
-          </div>
-          <button type="submit" className="btn btn-info btn-sm col-1">
-            Go
-          </button>
-        </div>
-      </form>
+      <Form />
+
       <div className="row">
         <div className="container-gif offset-1 col-6">
-          <img src="" alt="random gift" className="gif" />
+          <ImageDisplay text={text} />
         </div>
       </div>
       <div className="row">
